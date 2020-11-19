3b16c3303f18c21a4f32e226a6f74d0e0daa8f6a
karolwasemann <kw.kwaq@gmail.com>
2020-11-19 13:59:47 +0100

    feat: for & foreach

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-import '../scss/main.scss';
+import "../scss/main.scss";
 
 // uncomment the lines below to enable PWA
 // import {registerSW} from './pwa.js';
@@ -6,4 +6,26 @@ import '../scss/main.scss';
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+const btn = document.querySelector("button");
+const liItems = document.querySelectorAll("li");
+
+let fontSize = 10;
+
+// const add = function () {
+//   console.log("działa");
+//   for (let i = 0; i < 10; i++) {
+//     liItems[i].style.fontSize = `${fontSize}px`;
+//     liItems[i].style.display = "block";
+//   }
+//   fontSize += 4;
+// };
+
+// btn.addEventListener("click", add);
+
+btn.addEventListener("click", function () {
+  liItems.forEach((li) => {
+    li.style.display = "block";
+    li.style.fontSize = `${fontSize}px`;
+  });
+  fontSize++;
+});
